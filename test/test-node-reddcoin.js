02c4fa47fcbebc4e070ec89b24189526a7e500c1
Sam Vaughton <samvaughton@gmail.com>
2014-05-01 23:27:26 +0100

    Renamed the test file.

--- test/test-node-reddcoin.js
+++ test/test-node-reddcoin.js
@@ -3,7 +3,7 @@ var fs = require('fs'),
 
 var config = {
     rpchost: "127.0.0.1",
-    rpcport: 44555,
+    rpcport: 45443,
     rpcuser: "testnet_user",
     rpcpassword: "testnet_pass"
 };
@@ -17,7 +17,7 @@ options = {
       https: true,
       ca: ca
     };
-var dogecoin = require('../lib/dogecoin')(options);
+var reddcoin = require('../lib/reddcoin')(options);
 
 exports.get = function (test) {
 	var options_keys = Object.keys(options);
@@ -33,7 +33,7 @@ exports.get = function (test) {
 
 	var idx = 0;
 	for(var propt in options){
-		test.deepEqual(dogecoin.get(''+options_keys[idx]), options[''+propt]);
+		test.deepEqual(reddcoin.get(''+options_keys[idx]), options[''+propt]);
 		idx ++;
 	}
 	
@@ -43,7 +43,7 @@ exports.set = function (test) {
 
 	var new_options = {
       host: '133.7.7.7',
-      port: 22556,
+      port: 45443,
       user: 'new_1337_^*)()',
       pass: '*&@#cra$%zy@',
       passphrasecallback: function () { return 1+1;},
@@ -62,8 +62,8 @@ exports.set = function (test) {
 
 	var idx = 0;
 	for(var propt in new_options){
-		dogecoin.set(''+options_keys[idx], new_options[ ''+options_keys[idx] ]);
-		test.deepEqual(dogecoin.get(''+options_keys[idx]), new_options[''+propt]);
+		reddcoin.set(''+options_keys[idx], new_options[ ''+options_keys[idx] ]);
+		test.deepEqual(reddcoin.get(''+options_keys[idx]), new_options[''+propt]);
 		idx ++;
 	}
 	test.done();
@@ -76,20 +76,18 @@ exports.set = function (test) {
 
 /*
 /* BEFORE RUNNING read below:
- * Either run dogecoind directly or run dogecoin-qt with the -server
- * command line option. Make sure you have a ~/.dogecoin/dogecoin.conf
- * with rpcuser and rpcpassword config values filled out. Note that
- * newer versions of dogecoin (1.5 and above) don't allow identical
- * your rpc username and password to be identical.
+ * Either run reddcoind directly or run reddcoin-qt with the -server
+ * command line option. Make sure you have a ~/.reddcoin/reddcoin.conf
+ * with rpcuser and rpcpassword config values filled out.
  *
  */
 /*
 exports.commands_noAuth = {		
 	//NOTE: Before running the getBalance test add some testdoge to your wallet here: http://testdoge.lionservers.de/
-	// 			or add "gen=1" to the bottom of your dogecoin.conf file
+	// 			or add "gen=1" to the bottom of your reddcoin.conf file
 	getBalance: function(test){
 		var curr_balance;
-		dogecoin.getBalance(function(err, balance) {
+		reddcoin.getBalance(function(err, balance) {
 			test.ifError(err);
 		  if (err) {
 		    console.error('Failed to fetch balance', err.message);
@@ -105,19 +103,19 @@ exports.commands_noAuth = {
 
 	getGenerate: function(test){
 		test.expect(2);
-		dogecoin.setGenerate(true,1);	
-		test.equal(dogecoin.getGenerate(), true);
+		reddcoin.setGenerate(true,1);
+		test.equal(reddcoin.getGenerate(), true);
 
-		dogecoin.setGenerate(false,1);	
-		test.equal(dogecoin.getGenerate(), false);
+		reddcoin.setGenerate(false,1);
+		test.equal(reddcoin.getGenerate(), false);
 		test.done();
 	},
 	getreceived_: function(test){
 		var amount= 0.0001;
-		//dogecoin.setAccount()
-		sendfrom("testnet_user", dogecoin.getaccountaddress('testnet_user'),amount, function(err,addr){
+		//reddcoin.setAccount()
+		sendfrom("testnet_user", reddcoin.getaccountaddress('testnet_user'),amount, function(err,addr){
 			test.equal(getreceivedbyaccount('testnet_user', amount);
-			test.equal( getreceivedbyaddress( dogecoin.getaccountaddress('testnet_user') ), amount);
+			test.equal( getreceivedbyaddress( reddcoin.getaccountaddress('testnet_user') ), amount);
 			test.done();
 
 		});
@@ -133,7 +131,7 @@ exports.commands_noAuth = {
 exports.commands_Auth = {		
 	
 	setUp: function () {
-		dogecoin.auth('testnet_user', 'testnet_pass');
+		reddcoin.auth('testnet_user', 'testnet_pass');
 	}
 
 	
